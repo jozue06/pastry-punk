46514cad87353d522a804b0416c8ef3d45c575c6
Haley Mendoza <mendozahaley@gmail.com>
2018-02-24 10:55:27 -0800

    auto img slider working and allows manual input. need to work on button styles

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,18 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName('main');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1;}
+  x[slideIndex-1].style.display = 'block';
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
