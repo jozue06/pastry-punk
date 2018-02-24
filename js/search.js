53d3e385bf5c47d91aa33c5fb5566f7999568d22
Jozue06 <jozue06@gmail.com>
2018-02-23 23:53:11 -0800

    still fidling

--- js/search.js
+++ js/search.js
@@ -1,7 +1,17 @@
 'use strict';
 
-var validate = function() {
+var doSearch = function() {
   var result = document.getElementById('searchKey').value;
+  var strItems = JSON.stringify(result);
+  localStorage.setItem('searchKey', strItems);
   console.log('Your name is ' + result);
-
+  return result;
 };
+
+
+
+// for (var i = 0; i < allStores[0].data.length; i++){
+//   if (result === i)
+//     testSearchResults.push(result);
+//   console.log(testSearchResults);
+// }
