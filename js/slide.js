e74d6f9e7ae7a01beb5936a2aafa12bc65167ab0
Haley Mendoza <mendozahaley@gmail.com>
2018-02-24 09:47:44 -0800

    Changed feature pages layout. Added html for other posts.

--- js/slide.js
+++ js/slide.js
@@ -9,8 +9,8 @@ function showDivs(n) {
   var i;
   var x = document.getElementsByClassName('main');
   if (n > x.length) {slideIndex = 1;}
-  if (n < 1) {slideIndex = x.length;} 
-    for (i = 0; i < x.length; i++) {
+  if (n < 1) {slideIndex = x.length;}
+  for (i = 0; i < x.length; i++) {
     x[i].style.display = 'none';
   }
   x[slideIndex-1].style.display = 'block';
