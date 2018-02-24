dd47c07161478db74c3f5c8181192803fb787338
Jozue06 <jozue06@gmail.com>
2018-02-24 12:15:29 -0800

    search function working, need to display results

--- js/search.js
+++ js/search.js
@@ -1,17 +1,1 @@
 'use strict';
-
-var doSearch = function() {
-  var result = document.getElementById('searchKey').value;
-  var strItems = JSON.stringify(result);
-  localStorage.setItem('searchKey', strItems);
-  console.log('Your name is ' + result);
-  return result;
-};
-
-
-
-// for (var i = 0; i < allStores[0].data.length; i++){
-//   if (result === i)
-//     testSearchResults.push(result);
-//   console.log(testSearchResults);
-// }
