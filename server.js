4d67c15559d4d676714c2cf67474c147c2ac514a
Jozue06 <jozue06@gmail.com>
2018-05-08 23:19:26 -0700

    added postgres database from json info, pushed up to heroku

--- server.js
+++ server.js
@@ -22,67 +22,16 @@ app.use(express.static('public'));
 app.listen(port);
 
 
-// REVIEW: This helper function will load authors into the DB if the DB is empty.
-function loadLocations() {
-  fs.readFile('./public/data/stores.json', 'utf8', (err, fd) => {
-    JSON.parse(fd).forEach(ele => {
-      console.log(ele.name);
-      client.query(
 
-        'INSERT INTO locations (name, neighborhood, locationtype, globalinfluence, "website", hours, address, phone, "maptag", img, "mainpic", "minitop", "minibottom", blurb, pastry, item ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) ON CONFLICT DO NOTHING;',
-
-        [ele.name,
-          ele.neighborhood,
-          ele.locationtype,
-          ele.globalinfluence,
-          ele.website,
-          ele.hours,
-          ele.address,
-          ele.phone,
-          ele.maptag,
-          ele.img,
-          ele.mainpic,
-          ele.minitop,
-          ele.minibottom,
-          ele.blurb,
-          ele.pastry,
-          ele.item
-        ]
-      );
-    });
-  });
-}
-
-
-function loadDB() {
-  client.query(`
-      CREATE TABLE IF NOT EXISTS
-      locations (
-        name VARCHAR (255),
-        neighborhood VARCHAR(255), 
-        locationtype VARCHAR(255), 
-        globalinfluence VARCHAR(255), 
-        "website" VARCHAR (255), 
-        hours VARCHAR (255), 
-        address VARCHAR (255), 
-        phone VARCHAR (255), 
-        "maptag" VARCHAR, 
-        img VARCHAR (255), 
-        "mainpic" VARCHAR (255), 
-        "minitop" VARCHAR (255), 
-        "minibottom" VARCHAR (255), 
-        blurb text, 
-        pastry text, 
-        item text
-      );`
-  )
-    .then(console.log('stuff happend'))
-    .then(data => {
-      loadLocations(data);
+app.get('/test', (request, response) => {
+  client.query('SELECT * FROM locations;')
+    .then(result => {
+      response.send(result.rows);
     })
     .catch(err => {
       console.error(err);
     });
-}
+});
 
-loadDB();
+
+/* next up is to "get" from the database on page load, parse to json, and store in local storage like the other "get" for the google sheets was doing. then search etc. will work as it did before */
